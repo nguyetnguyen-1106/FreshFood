0322ce12ecd13add4d54d7d21c4b2120f1117340
nguyetnguyen-1106 <54876321+nguyetnguyen-1106@users.noreply.github.com>
2019-10-31 21:46:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -124,13 +124,13 @@ function list_Vegetable(){
 					document.getElementById("containerCart").innerHTML = "";
 				}
 			}(i);
-			line.appendChild(prImage);
-			line.appendChild(prName);
-			line.appendChild(prPrice);
-			line.appendChild(btAdd);
-			vegetable.appendChild(line);
-			vegetable.className = "line";
-			line.className = "border-Pro";
+		line.appendChild(prImage);
+		line.appendChild(prName);
+		line.appendChild(prPrice);
+		line.appendChild(btAdd);
+		vegetable.appendChild(line);
+		vegetable.className = "line";
+		line.className = "border-Pro";
 	}		
 	document.getElementById("fruit").style.display = "none";
 	document.getElementById("vegetable").style.display = "grid";
@@ -176,13 +176,13 @@ function list_Fruits(){
 					document.getElementById("containerCart").innerHTML = "";
 				}
 			}(i);
-			line.appendChild(prImage);
-			line.appendChild(prName);
-			line.appendChild(prPrice);
-			line.appendChild(btAdd);
-			fruit.appendChild(line);
-			fruit.className = "line";
-			line.className = "border-Pro";
+		line.appendChild(prImage);
+		line.appendChild(prName);
+		line.appendChild(prPrice);
+		line.appendChild(btAdd);
+		fruit.appendChild(line);
+		fruit.className = "line";
+		line.className = "border-Pro";
 	}		
 	document.getElementById("fruit").style.display = "grid";
 	document.getElementById("vegetable").style.display = "none";
@@ -191,12 +191,11 @@ function list_Fruits(){
 	document.getElementById("container").style.display = "none";
 	document.getElementById("containerCart").style.display = "none";
 }
-
 function list_Foods(){
 	document.getElementById("food").innerHTML = "";
 	var food = document.getElementById('food');
 	for (var i = 0; i < foods.length; i++) {
-			var line = document.createElement("div");
+		var line = document.createElement("div");
 		var prName = document.createElement("p");
 			prName.innerText = foods[i].name;
 		var prPrice = document.createElement("p");
@@ -229,13 +228,13 @@ function list_Foods(){
 					document.getElementById("containerCart").innerHTML = "";
 				}
 			}(i);
-			line.appendChild(prImage);
-			line.appendChild(prName);
-			line.appendChild(prPrice);
-			line.appendChild(btAdd);
-			food.appendChild(line);
-			food.className = "line";
-			line.className = "border-Pro";
+		line.appendChild(prImage);
+		line.appendChild(prName);
+		line.appendChild(prPrice);
+		line.appendChild(btAdd);
+		food.appendChild(line);
+		food.className = "line";
+		line.className = "border-Pro";
 	}		
 	document.getElementById("fruit").style.display = "none";
 	document.getElementById("vegetable").style.display = "none";
@@ -243,9 +242,7 @@ function list_Foods(){
 	document.getElementById("convenience").style.display = "none";
 	document.getElementById("container").style.display = "none";
 	document.getElementById("containerCart").style.display = "none";
-
 }
-
 function list_Convenience(){
 	document.getElementById("convenience").innerHTML = "";
 	var convenience = document.getElementById('convenience');
@@ -283,13 +280,13 @@ function list_Convenience(){
 					document.getElementById("containerCart").innerHTML = "";
 				}
 			}(i);
-			line.appendChild(prImage);
-			line.appendChild(prName);
-			line.appendChild(prPrice);
-			line.appendChild(btAdd);
-			convenience.appendChild(line);
-			convenience.className = "line";
-			line.className = "border-Pro";
+		line.appendChild(prImage);
+		line.appendChild(prName);
+		line.appendChild(prPrice);
+		line.appendChild(btAdd);
+		convenience.appendChild(line);
+		convenience.className = "line";
+		line.className = "border-Pro";
 	}		
 	document.getElementById("fruit").style.display = "none";
 	document.getElementById("vegetable").style.display = "none";
@@ -334,20 +331,19 @@ function displayProduct(){
 					document.getElementById("containerCart").innerHTML = "";
 				}
 			}(i);
-			line.appendChild(prImage);
-			line.appendChild(prName);
-			line.appendChild(prPrice);
-			line.appendChild(btAdd);
-			container.appendChild(line);
-			container.className = "line";
-			line.className = "border-Pro";
-		}
-		document.getElementById("fruit").style.display = "none";
-		document.getElementById("vegetable").style.display = "none";
-		document.getElementById("container").style.display = "grid";
-		document.getElementById("containerCart").style.display = "none";
+		line.appendChild(prImage);
+		line.appendChild(prName);
+		line.appendChild(prPrice);
+		line.appendChild(btAdd);
+		container.appendChild(line);
+		container.className = "line";
+		line.className = "border-Pro";
 	}
-
+	document.getElementById("fruit").style.display = "none";
+	document.getElementById("vegetable").style.display = "none";
+	document.getElementById("container").style.display = "grid";
+	document.getElementById("containerCart").style.display = "none";
+}
 function displayCart(){
 	document.getElementById("containerCart").innerHTML = "";
 	var containerCart = document.getElementById('containerCart');
@@ -383,15 +379,15 @@ function displayCart(){
 						Info_Cus();
 					}
 				}(i)
-			line.appendChild(prImage);
-			line.appendChild(prName);
-			line.appendChild(prPrice);
-			line.appendChild(quantity);
-			line.appendChild(btDelete);
-			line.appendChild(btOrder);
-			containerCart.appendChild(line);
-			line.className = "line";
-		}
+		line.appendChild(prImage);
+		line.appendChild(prName);
+		line.appendChild(prPrice);
+		line.appendChild(quantity);
+		line.appendChild(btDelete);
+		line.appendChild(btOrder);
+		containerCart.appendChild(line);
+		line.className = "line";
+	}
 	document.getElementById("container").style.display = "none";
 	document.getElementById("vegetable").style.display = "none";
 	document.getElementById("fruit").style.display = "none";
@@ -401,92 +397,79 @@ function displayCart(){
 }
 function Info_Cus(){
 	document.getElementById('containerCart').innerHTML = "";
-			var line = document.createElement("div");
-
-			var customer = document.createElement("h1");
-				customer.innerText = "Thông tin khách hàng";
-
-			var name = document.createElement("p");
-				name.innerText = "Họ và tên";
-			var inputName = document.createElement("input");
-				inputName.innerText = " ";
-				inputName.className = "form";
-
-			var address = document.createElement("p");
-				address.innerText = "Địa chỉ";
-			var inputAddress = document.createElement("input");
-				inputAddress.innerText = " ";
-				inputAddress.className = "form";
-
-			var phone = document.createElement("p");
-				phone.innerText = "Số điện thoại";
-			var inputPhone = document.createElement("input");
-				inputPhone.innerText = " ";
-				inputPhone.className = "form";
-
-			var email = document.createElement("p");
-				email.innerText = "Email";
-			var inputEmail = document.createElement("input");
-				inputEmail.innerText = " ";
-				inputEmail.className = "form";
-			var line2 = document.createElement("div");
-
-			var pay = document.createElement("button");
-				pay.innerText = "Thanh toán"
-				pay.onclick = function(){
-					if(inputName.value == "" || inputAddress.value == "" || inputEmail.value == "" || inputPhone.value == ""){
-						alert("Mời bạn nhập đầy đủ thông tin");
-					}
-					else{
-						alert("Bạn đã đặt hàng thành công");
-					}
-				}
-			var product = document.createElement("h1");
-				product.innerText = "Xem lại đơn hàng";
-
-		for (var i = 0; i < cart.length;  i++) {
-			var line1 = document.createElement("div");
-
-			var prName = document.createElement("h2");
-				prName.innerText = cart[i].name;
-
-			var prPrice = document.createElement("h2");
-				prPrice.innerText = "Giá: " + cart[i].price;
-
-			var quantity = document.createElement("h2");
-				quantity.innerText = "Số lượng: " + cart[i].quantity;
-
-			var prImage = document.createElement("img");
-				prImage.src= cart[i].image;
-				prImage.className = "image";
-
-			var totalPrice = document.createElement("h2");
-				totalPrice.innerText += "Tổng: " + (cart[i].price * cart[i].quantity);
+	var line = document.createElement("div");
+	var customer = document.createElement("h1");
+	customer.innerText = "Thông tin khách hàng";
+	var name = document.createElement("p");
+		name.innerText = "Họ và tên";
+	var inputName = document.createElement("input");
+		inputName.innerText = " ";
+		inputName.className = "form";
+	var address = document.createElement("p");
+		address.innerText = "Địa chỉ";
+	var inputAddress = document.createElement("input");
+		inputAddress.innerText = " ";
+		inputAddress.className = "form";
+	var phone = document.createElement("p");
+		phone.innerText = "Số điện thoại";
+	var inputPhone = document.createElement("input");
+		inputPhone.innerText = " ";
+		inputPhone.className = "form";
+	var email = document.createElement("p");
+		email.innerText = "Email";
+	var inputEmail = document.createElement("input");
+		inputEmail.innerText = " ";
+		inputEmail.className = "form";
+	var line2 = document.createElement("div");
+	var pay = document.createElement("button");
+		pay.innerText = "Thanh toán"
+		pay.onclick = function(){
+			if(inputName.value == "" || inputAddress.value == "" || inputEmail.value == "" || inputPhone.value == ""){
+				alert("Mời bạn nhập đầy đủ thông tin");
+			}
+			else{
+				alert("Bạn đã đặt hàng thành công");
+			}
 		}
-			line.appendChild(customer);
-			line.appendChild(name);
-			line.appendChild(inputName);
-			line.appendChild(address);
-			line.appendChild(inputAddress);
-			line.appendChild(phone);
-			line.appendChild(inputPhone);
-			line.appendChild(email);
-			line.appendChild(inputEmail);
-			line1.appendChild(product);
-			line1.appendChild(prImage);
-			line1.appendChild(prName);
-			line1.appendChild(prPrice);
-			line1.appendChild(quantity);
-			line1.appendChild(totalPrice);
-			line2.appendChild(pay)
-			containerCart.appendChild(line);
-			containerCart.appendChild(line1);
-			containerCart.appendChild(line2);
-			containerCart.className = "line";
-			inputName.className = "form";
+	var product = document.createElement("h1");
+		product.innerText = "Xem lại đơn hàng";
+	for (var i = 0; i < cart.length;  i++) {
+		var line1 = document.createElement("div");
+		var prName = document.createElement("h2");
+			prName.innerText = cart[i].name;
+		var prPrice = document.createElement("h2");
+			prPrice.innerText = "Giá: " + cart[i].price;
+		var quantity = document.createElement("h2");
+			quantity.innerText = "Số lượng: " + cart[i].quantity;
+		var prImage = document.createElement("img");
+			prImage.src= cart[i].image;
+			prImage.className = "image";
+		var totalPrice = document.createElement("h2");
+			totalPrice.innerText += "Tổng: " + (cart[i].price * cart[i].quantity);
+	}
+	line.appendChild(customer);
+	line.appendChild(name);
+	line.appendChild(inputName);
+	line.appendChild(address);
+	line.appendChild(inputAddress);
+	line.appendChild(phone);
+	line.appendChild(inputPhone);
+	line.appendChild(email);
+	line.appendChild(inputEmail);
+	line1.appendChild(product);
+	line1.appendChild(prImage);
+	line1.appendChild(prName);
+	line1.appendChild(prPrice);
+	line1.appendChild(quantity);
+	line1.appendChild(totalPrice);
+	line2.appendChild(pay)
+	containerCart.appendChild(line);
+	containerCart.appendChild(line1);
+	containerCart.appendChild(line2);
+	containerCart.className = "line";
+	inputName.className = "form";
 	document.getElementById("containerCart").style.display = "grid";
 }
-
 var research = []
 function search(){
 	var a = document.getElementById("sea").value;
